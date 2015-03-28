df499690e9901d1ea26b305f30ef0e2808f583e9
Kirstin "Kir" Jarchow <kirstinjarchow@gmail.com>
2015-03-28 10:19:40 -0700

    Display time prior to setTimeout()

--- src/app.js
+++ src/app.js
@@ -4,9 +4,10 @@ var date = new Date();
 
 var window = new UI.Window();
 var textfield = new UI.Text({
-  position: new Vector2(10, 40),
+  position: new Vector2(30, 40),
   size: new Vector2(144, 168),
-  font: 'gothic-18-bold'
+  font: 'gothic-18-bold',
+  text: "00000000\n00000000\n00000000"
 });
 
 window.add(textfield);
@@ -26,6 +27,9 @@ var getBinaryTime = function(){
   return time.join('\n');
 };
 
+textfield.text(getBinaryTime());
+
 setInterval(function(){
+  console.log("a second has passed");
   textfield.text(getBinaryTime());
 }, 1000);
